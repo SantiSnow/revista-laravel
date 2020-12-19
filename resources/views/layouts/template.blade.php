65ecb1f1ee6c94757e814aa22e8ebde12ac73184
Santiago Aguirre <aguirresantiago81@gmail.com>
2020-12-19 13:10:56 -0300

    Cambios en los CDN de Bootstrap, Jquery, Popper y otros de JS

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -9,8 +9,10 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <!-- iconos de google -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!-- link href="css/bootstrap.solar.min.css" rel="stylesheet" -->
+    <!-- Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <style>
         #mi-form-busqueda{
             margin: 15px;
@@ -62,7 +64,7 @@
             <li class="nav-item"> <a class="nav-link" href="{{ url('/articulos-programacion') }}">Cursos de informática </a> </li>
             <li class="nav-item"> <a class="nav-link" href="{{ url('/sobreNosotros') }}">Sobre Nosotros</a> </li>
         </ul>
-        
+
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
@@ -138,8 +140,8 @@
 <div class="container-fluid">
     @yield('contenido')
 
-    
-    
+
+
 </div>
 
 <br />
@@ -173,7 +175,7 @@
                     <textarea rows="10" cols="100" class="form-control" id="mensaje" name="mensaje" placeholder="Duda, consulta o peticion" aria-describedby="messageHelp"></textarea>
                     <span id="messageHelp" class="form-text text-muted" style="display: none;">Escribi tu mensaje.</span>
                   </div>
-                    
+
                     <!--button class="btn btn-primary" type="submit">¡Enviar!</button>
                     <br />
                     <br />
@@ -200,7 +202,7 @@
                     <div class="col-sm-4 col-md-4 col-lg-4 col-xs-6">
                         <div>
                             <ul class="list-unstyled">
-                                <li class="fondoBlancoRed"> <a href="https://www.facebook.com/CompuItuzaingo/?ref=bookmarks"> 
+                                <li class="fondoBlancoRed"> <a href="https://www.facebook.com/CompuItuzaingo/?ref=bookmarks">
                                 <img alt="facebook" src="../images/facebook.png" width="40px" height="40px" class="iconosOscuros" style="border-radius: 12px;"> </a> </li>
                             </ul>
                         </div>
@@ -208,7 +210,7 @@
                     <div class="col-sm-4 col-md-4 col-lg-4  col-xs-6">
                         <div>
                             <ul class="list-unstyled">
-                                <li class="fondoBlancoRed"> <a href="mailto:noticiascinformatica@gmail.com?Subject=Contacto%20Desde%20El%20Sitio%20Web%20Consulta"> 
+                                <li class="fondoBlancoRed"> <a href="mailto:noticiascinformatica@gmail.com?Subject=Contacto%20Desde%20El%20Sitio%20Web%20Consulta">
                                 <img alt="gmail" src="../images/gmail.png" width="40px" height="35px" class="iconosOscuros" style="border-radius: 8px;"></a> </li>
                             </ul>
                         </div>
@@ -223,7 +225,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-5"> 
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-5">
                 <address>
                 <strong>Creado por CompuItuzaingo</strong><br />
                 CISoftware - Creación de sistemas web.<br />
@@ -250,9 +252,9 @@
 <!-- modo nocturno script -->
 <script src="../modoNoc.js"></script>
 
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<!-- Popper y Bootstrap JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 </body>
 </html>
